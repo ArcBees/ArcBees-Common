19625e67320a98893c1bf0b476d8a34205d0ca7e
albre <bretn.alexandr@gmail.com>
2013-08-13 15:53:58 -0400

    CR fix

--- websiteassets/src/main/java/com/arcbees/website/assets/client/application/forkOnGithub/ForkOnGithubWidget.java
+++ websiteassets/src/main/java/com/arcbees/website/assets/client/application/forkOnGithub/ForkOnGithubWidget.java
@@ -1,7 +1,9 @@
 package com.arcbees.website.assets.client.application.forkOnGithub;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.AnchorElement;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -11,7 +13,14 @@ public class ForkOnGithubWidget extends Composite {
 
     private static ForkOnGithubWidgetUiBinder ourUiBinder = GWT.create(ForkOnGithubWidgetUiBinder.class);
 
+    @UiField
+    AnchorElement link;
+
     public ForkOnGithubWidget() {
         initWidget(ourUiBinder.createAndBindUi(this));
+    }
+
+    public void setLinkHref(String href) {
+        link.setHref(href);
     }
 }
